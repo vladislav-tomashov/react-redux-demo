a497021cae2a2f108605e7842e6e433d83fe8200
Vlad Tomashov <tomashov.v@dbbest.com>
2018-11-21 11:31:08 +0200

    LoadButton done
    Signed-off-by: Vlad Tomashov <tomashov.v@dbbest.com>

--- src/components/LoadButton/LoadButton.js
+++ src/components/LoadButton/LoadButton.js
@@ -1,17 +1,65 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import "./LoadButton.css";
+import { loadCurrencyRates } from "../../store/actions/currencyRates/currencyRatesActions";
+import {
+  isCurrencyRatesLoading,
+  getCurrencyRates
+} from "../../store/selectors/currencyRatesSelectors";
 
 class LoadButton extends Component {
+  static propTypes = {
+    loadCurrencyRates: PropTypes.func.isRequired,
+    loading: PropTypes.bool,
+    loaded: PropTypes.bool
+  };
   constructor(props) {
     super(props);
     this.ref = React.createRef();
   }
-  onClick = () => {
-    console.log("click!");
+  focus = () => {
+    this.ref.current.focus();
   };
-  render() {
-    return <button onClick={this.onClick}> Load rates</button>;
-  }
+  onClick = () => {
+    this.props.loadCurrencyRates();
+  };
+  componentDidMount = () => {
+    this.focus();
+  };
+  render = () => {
+    let label = "Load rates";
+    if (this.props.loading) {
+      label = "Loading...";
+    } else if (this.props.loaded) {
+      label = "Update rates";
+    }
+    return (
+      <button
+        disabled={this.props.loading}
+        ref={this.ref}
+        onClick={this.onClick}
+      >
+        {label}
+      </button>
+    );
+  };
 }
 
-export { LoadButton as default };
+const mapStateToProps = (state /*, ownProps*/) => {
+  return {
+    loading: isCurrencyRatesLoading(state),
+    loaded: !!getCurrencyRates(state)
+  };
+};
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ loadCurrencyRates }, dispatch);
+
+const ConnectedLoadButton = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoadButton);
+
+export { ConnectedLoadButton as default, LoadButton };
