98a6328fd2477292598a6bc5a3f4f34230dda0f4
Vlad Tomashov <tomashov.v@dbbest.com>
2018-11-23 13:47:28 +0200

    adds jest-fetch-mock, redux-mocj-store for testing
    Signed-off-by: Vlad Tomashov <tomashov.v@dbbest.com>

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import "jest-enzyme";
 
+global.fetch = require("jest-fetch-mock");
 configure({ adapter: new Adapter() });
